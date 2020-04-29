94bc57b0dd5644682b1fb0c689fb43b6b9916170
Руслан Васильев <rusvas15@yandex.ru>
2020-04-29 17:30:06 +0300

    Перестал использовать доп файл. Сократил код

--- lw17/17-1.pas
+++ lw17/17-1.pas
@@ -2,13 +2,12 @@ PROGRAM SumDigit(INPUT, OUTPUT);
 VAR
   Sum, Number: INTEGER;
   Ch: CHAR;
-  F1: TEXT;
 PROCEDURE ReadDigit(VAR F: TEXT; VAR D: INTEGER);
 BEGIN
   IF NOT EOLN(F)
   THEN
     READ(F, Ch);
-    D := 0;
+    D := -1;
     IF Ch = '0' THEN D := 0 ELSE
     IF Ch = '1' THEN D := 1 ELSE
     IF Ch = '2' THEN D := 2 ELSE
@@ -18,34 +17,18 @@ BEGIN
     IF Ch = '6' THEN D := 6 ELSE
     IF Ch = '7' THEN D := 7 ELSE
     IF Ch = '8' THEN D := 8 ELSE
-    IF Ch = '9' THEN D := 9 
-    ELSE
-      WHILE NOT EOLN(F)
-      DO
-        READ(F, Ch)               
+    IF Ch = '9' THEN D := 9             
 END;  
 BEGIN {SumDigit}
+  Number := 0;
   Sum := 0;
-  IF NOT EOLN(INPUT)
-  THEN
-    READ(INPUT, Ch);
-  IF (Ch >= '0') AND (Ch <= '9')
-  THEN 
-    BEGIN  
-      REWRITE(F1);       
-      WRITE(F1, Ch);
-      ReadDigit(F1, Number);          
-      Sum := Sum + Number; 
-      WHILE NOT EOLN(INPUT) AND (Ch >= '0') AND (Ch <= '9')
-      DO
-        BEGIN            
-          READ(INPUT, Ch);
-          WRITE(F1, Ch); 
-          ReadDigit(F1, Number);          
-          Sum := Sum + Number               
-        END
-    END    
-  ELSE
-    Sum := -1;      
-  WRITELN(OUTPUT, Sum)      
+  WHILE (NOT EOLN) AND (Number <> -1)
+  DO
+    BEGIN
+      ReadDigit(INPUT, Number);
+      IF Number <> -1
+      THEN
+        Sum := Sum + Number
+    END;
+  WRITELN(Sum)      
 END. {SumDigit}
