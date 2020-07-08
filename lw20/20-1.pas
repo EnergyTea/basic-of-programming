22077d0998ca55d260c2ebd55dc33e69f4d09740
Руслан Васильев <rusvas15@yandex.ru>
2020-07-08 14:30:20 +0300

    перезалил

--- lw20/20-1.pas
+++ lw20/20-1.pas
@@ -1,47 +1,46 @@
-PROGRAM TreeSort(INPUT, OUTPUT);
-TYPE 
-  Tree = ^NodeType;
-  NodeType = RECORD
-               Ch: CHAR;
-               LLink, RLink: Tree;
-             END;
+PROGRAM XPrint(INPUT, OUTPUT);
+CONST
+  MIN = 1;
+  MAX = 25;
+TYPE
+  Display = SET OF MIN..MAX;
 VAR
-  Root: Tree;
-  Ch: CHAR;
-PROCEDURE Insert(VAR Ptr:Tree; Data: CHAR);
-BEGIN {Insert}
-  IF Ptr = NIL
-  THEN
-    BEGIN
-      NEW(Ptr);
-      Ptr^.Ch := Data;
-      Ptr^.LLink := NIL;
-      Ptr^.RLink := NIL;
-    END
-  ELSE
-    IF Data <= Ptr^.Ch
-    THEN
-      Insert(Ptr^.LLink, Data)
-    ELSE
-      Insert(Ptr^.RLink, Data)
-END;  {Insert}
-PROCEDURE PrintTree(Ptr: Tree);
-BEGIN {PrintTree}
-  IF Ptr <> NIL
-  THEN 
-    BEGIN
-      PrintTree(Ptr^.LLink);
-      WRITE(Ptr^.Ch);
-      PrintTree(Ptr^.RLink);
-    END;
-END;  {PrintTree}
-BEGIN {TreeSort}
-  Root := NIL;
-  WHILE NOT EOLN
+  Cursor: INTEGER;
+  Model: CHAR;    
+  Matrix: Display;
+PROCEDURE Identif(VAR Model: CHAR; VAR Matrix: Display);
+BEGIN {Identif} 
+  CASE Model OF
+    'R': Matrix := [1, 2, 3, 6, 8, 11, 12, 16, 18, 21, 24];
+    'U': Matrix := [1, 5, 6, 10, 11, 15, 16, 20, 22, 23, 24];
+    'S': Matrix := [1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 20, 21, 22, 23, 24, 25];
+    'L': Matrix := [1, 6, 11, 16, 21, 22, 23, 24, 25];
+    'A': Matrix := [5, 9, 10, 13, 15, 17, 18, 19, 20, 21, 25];
+    'N': Matrix := [1, 5, 6, 7, 10, 11, 13, 15, 16, 19, 20, 21, 25];
+  END
+END; {Identif}  
+PROCEDURE Print(VAR Matrix: Display);
+BEGIN
+  FOR Cursor := MIN TO MAX
   DO
     BEGIN
-      READ(Ch);
-      Insert(Root, Ch)
-    END;
-  PrintTree(Root)
-END.  {TreeSort}
+      IF (Cursor IN Matrix)
+      THEN
+        WRITE(OUTPUT, 'X')
+      ELSE
+        WRITE(OUTPUT, ' ');
+      IF (Cursor MOD 5 = 0)
+      THEN
+        WRITELN
+    END; 
+END;    
+BEGIN
+  IF NOT EOLN
+  THEN
+    BEGIN
+      READ(Model);
+      Identif(Model, Matrix);
+      Print(Matrix);
+    END;  
+  WRITELN
+END. 
